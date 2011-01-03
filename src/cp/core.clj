3a3a4d766463b39475cad81e0774255ad81a4607
takkanm <takkanm@gmail.com>
2011-01-03 17:59:59 +0900

    cpを実装

--- src/cp/core.clj
+++ src/cp/core.clj
@@ -1,5 +1,10 @@
 (ns cp.core
   (:gen-class))
+(use '[clojure.contrib.duck-streams :only (reader writer copy)])
+
+(defn copy-file [src-file dst-file]
+  (with-open [src (reader src-file) dst (writer dst-file)]
+    (copy src dst)))
 
 (defn -main [& args]
-  (println "hello"))
+  (copy-file (first args) (second args)))
